1cf63c4edf61ccec8b85a9dba5202e902223cf7a
Daniell Castelo Branco <danicbciriaco2@gmail.com>
2021-08-24 12:53:38 -0300

    type annotations adicionadas

--- Classes/dado.py
+++ Classes/dado.py
@@ -2,9 +2,9 @@ from random import randint
 
 
 class Dado:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.valor_minimo = 1
 		self.valor_maximo = 6
 	
-	def sortear(self):
+	def sortear(self) -> int:
 		return randint(self.valor_minimo, self.valor_maximo)
