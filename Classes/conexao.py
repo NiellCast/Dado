db3d2869d30b6083632f7707341acd7a9ab16a6f
Daniell Castelo Branco <danicbciriaco2@gmail.com>
2021-08-24 13:29:52 -0300

    classe de conexao com o telegram criada

---
+++ Classes/conexao.py
@@ -0,0 +1,43 @@
+from os import getenv
+from telepot import Bot
+from time import sleep
+from dotenv import load_dotenv
+from Classes.mensagens import Gerenciamento
+from Classes.enviar_mensagem import Envio
+from Classes.dado import Dado
+
+
+class MsgRequest:
+	load_dotenv()
+	
+	def __init__(self) -> None:
+		self.__bot = Bot(getenv('TOKEN'))
+		self.__gerenciamento = Gerenciamento()
+		self.__mensagem = Envio()
+		self.num = Dado()
+		
+	def mensagens(self, msg: int or slice) -> None:
+		"""
+		:param msg: Recebe o que o usuário digitou e se a mensagem estiver entre as esperadasm, sorteia um número.
+		:return: Envia uuma imagem com o número sorteado para o usuário.
+		"""
+		
+		id_do_usuario = msg['from']['id']  # Guarda o ID do usuário que está utilizando o Bot.
+		
+		if not msg['from']['is_bot']:  # Verifica se o usuário não é um robô.
+			if self.__gerenciamento.validar(msg['text'].upper()):
+				self.__mensagem.enviar_texto(id_do_usuario, self.__gerenciamento.mensagens(0))
+				self.__mensagem.enviar_photo(id_do_usuario, self.num.sortear())
+				sleep(0.5)
+				self.__mensagem.enviar_texto(id_do_usuario, self.__gerenciamento.mensagens(1))
+			
+			else:
+				self.__mensagem.enviar_dica(id_do_usuario)
+			
+	def conectar(self) -> None:
+		"""
+		:return: Mantém a conexão com o telegram sem interrupções.
+		"""
+		self.__bot.message_loop(self.mensagens)
+		while True:
+			pass
--- Classes/enviar_mensagem.py
+++ Classes/enviar_mensagem.py
@@ -17,3 +17,7 @@ class Envio:
 	def enviar_photo(self, id_do_usuario: str, id_da_photo: int) -> None:
 		self.__bot.sendPhoto(id_do_usuario, photo=open(f'imagens/numero_{id_da_photo}.png', 'rb'))
 		sleep(0.5)
+	
+	def enviar_dica(self, id_do_usuario: str):
+		self.__bot.sendMessage(id_do_usuario, 'Envie "J" para jogar o dado!')
+		sleep(0.5)
