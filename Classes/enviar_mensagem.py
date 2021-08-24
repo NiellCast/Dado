08b3dad825fa81b936b6c3a3d2cfc9223f4f993c
Daniell Castelo Branco <danicbciriaco2@gmail.com>
2021-08-24 13:03:40 -0300

    classe para envio de mensagens criada

---
+++ Classes/enviar_mensagem.py
@@ -0,0 +1,19 @@
+from os import getenv
+from telepot import Bot
+from time import sleep
+from dotenv import load_dotenv
+
+
+class Envio:
+	load_dotenv()
+	
+	def __init__(self) -> None:
+		self.__bot = Bot(getenv('TOKEN'))
+		
+	def enviar_texto(self, id_do_usuario: str, mensagem: str) -> None:
+		self.__bot.sendMessage(id_do_usuario, mensagem)
+		sleep(0.5)
+
+	def enviar_photo(self, id_do_usuario: str, id_da_photo: int) -> None:
+		self.__bot.sendPhoto(id_do_usuario, photo=open(f'imagens/numero_{id_da_photo}.png', 'rb'))
+		sleep(0.5)
