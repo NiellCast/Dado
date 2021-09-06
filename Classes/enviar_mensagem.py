5e48b75f5b9bf3398d27557e1fe82495cf84f773
Daniell Castelo Branco <danicbciriaco2@gmail.com>
2021-09-06 12:29:21 -0300

    Docstrings adicionadas

--- Classes/enviar_mensagem.py
+++ Classes/enviar_mensagem.py
@@ -11,13 +11,30 @@ class Envio:
 		self.__bot = Bot(getenv('TOKEN'))
 		
 	def enviar_texto(self, id_do_usuario: str, mensagem: str) -> None:
+		"""
+		:param id_do_usuario: Recebe o id do usuário do Telegram.
+		:param mensagem: Recebe a mensagem a ser enviada.
+		:return: Envia a mensagem para o id recebido.
+		"""
+		
 		self.__bot.sendMessage(id_do_usuario, mensagem)
 		sleep(0.5)
 
 	def enviar_photo(self, id_do_usuario: str, id_da_photo: int) -> None:
+		"""
+		:param id_do_usuario: Recebe o id do usuário do Telegram.
+		:param id_da_photo: Recebe o id da foto sorteada.
+		:return: Envia a foto para o id recebido.
+		"""
+		
 		self.__bot.sendPhoto(id_do_usuario, photo=open(f'imagens/numero_{id_da_photo}.png', 'rb'))
 		sleep(0.5)
 	
 	def enviar_dica(self, id_do_usuario: str):
+		"""
+		:param id_do_usuario: Recebe o id do usuário do Telegram.
+		:return: Envia uma mensagem contendo uma dica de como iniciar o bot.
+		"""
+		
 		self.__bot.sendMessage(id_do_usuario, 'Envie "J" para jogar o dado!')
 		sleep(0.5)
