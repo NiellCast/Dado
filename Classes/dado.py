da41b8e62b0b7ba91cc2a2d8dc2e301105b7c046
Daniell Castelo Branco <danicbciriaco2@gmail.com>
2021-09-06 12:17:14 -0300

    Docstring adicionada na classe dado

--- Classes/dado.py
+++ Classes/dado.py
@@ -7,4 +7,8 @@ class Dado:
 		self.valor_maximo = 6
 	
 	def sortear(self) -> int:
+		"""
+		:return: Retorna um número aleatório de 1 a 6.
+		"""
+		
 		return randint(self.valor_minimo, self.valor_maximo)
