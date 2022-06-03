ce437174e6e96c1d639c6fac21f85322601c148f
Daniell Castelo Branco Ciriaco <danicbciriaco2@gmail.com>
2022-06-03 12:33:48 -0300

    Mudança de api concluída

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from Classes.conexao import MsgRequest
+from src.telegram_bot.bot import Bot
+from dotenv import load_dotenv
+from os import getenv
+from src.gerador_de_numeros.dado import Dado
 
 
 def run() -> None:
 	"""
 	:return: inicia o bot.
 	"""
-	
-	bot = MsgRequest()
-	
-	bot.conectar()
+
+	load_dotenv()
+	bot = Bot()
+
+	@bot.message_handler(commands=[getenv('COMANDO_ADM')])
+	def responder_adm(mensagem) -> None:
+		"""
+		:param mensagem: Recebe a mensagem enviada pelo aplicativo.
+		:return: Retorna quantas vezes o bot foi utilizado.
+		"""
+		print(mensagem.from_user.first_name)
+		print(mensagem.from_user.username)
+
+	@bot.message_handler(commands=['start', 'jogar'])
+	def responder_usuario(mensagem) -> None:
+		imagem = Dado.sortear_numero()
+
+		bot.send_photo(mensagem.chat.id, open(f'imagens/numero_{imagem}.png', 'rb'))
+
+	bot.polling()
 
 
 if __name__ == '__main__':
