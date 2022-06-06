840b3a36a55c285ac1fe44fc59090a961bd796d2
Daniell Castelo Branco Ciriaco <danicbciriaco2@gmail.com>
2022-06-06 11:53:50 -0300

    pacote para database e conector criado

---
+++ src/database/db.py
@@ -0,0 +1,29 @@
+from sqlite3 import connect
+from dotenv import load_dotenv
+from os import getenv
+from typing import List
+
+
+class Conector:
+    load_dotenv()
+
+    def __init__(self, db: str) -> None:
+        self.__conexao = connect(db, check_same_thread=False)
+        self.__cursor = self.__conexao.cursor()
+
+    def create(self, username: str) -> None:
+        with self.__conexao:
+            self.__cursor.execute(
+                f'INSERT INTO {getenv("TABLE")} ({getenv("COLUMN")}) VALUES (?)',
+                (username,)
+
+            )
+            self.__conexao.commit()
+
+    def read(self) -> List:
+        with self.__conexao:
+            self.__cursor.execute(
+                f'SELECT * FROM {getenv("TABLE")}'
+            )
+
+            return self.__cursor.fetchall()
